4b03f80cc8990bcbc1d8019e8405a3ce41ba222a
remi-boivin <remi.boivin@epitech.eu>
2023-02-11 00:29:52 +0100

    :white_check_mark: test(logger): Add logger tests
    check if the logger works as expected

---
+++ tests/includes/LoggerTest.hpp
@@ -0,0 +1,11 @@
+#ifndef LOGGER_TEST_HPP
+#define LOGGER_TEST_HPP
+
+#include <CppUTest/MemoryLeakDetectorNewMacros.h>
+#include <CppUTest/TestHarness.h>
+#include <Log/logger.hpp>
+#include <fstream>
+#include <iostream>
+#include <sstream>
+
+#endif // LOGGER_TEST_HPP
