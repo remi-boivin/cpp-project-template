10e12754e45e7288fb3b8298fa485f59d6feed2e
remi-boivin <remi.boivin@epitech.eu>
2023-02-11 00:26:15 +0100

    :sparkles: feat(log): add log feature
    add different level of loging. It log only if the level of the log is >= logLevel

--- includes/my.hpp
+++ includes/my.hpp
@@ -1,4 +1,6 @@
 #ifndef MY_HPP
 #define MY_HPP
 
+#include <Log/logger.hpp>
+
 #endif // MY_HPP
--- src/main.cpp
+++ src/main.cpp
@@ -2,5 +2,8 @@
 
 auto main() -> int
 {
+    Logger logger { LogSeverity::kNormal };
+    logger.log("This is a project's template !", LogSeverity::kNormal);
+
     return 0;
 }
